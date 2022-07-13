e196167428d4966258a5c3313b8e62a6fd162409
0pdo1Tall <silverbullet2206@gmail.com>
2022-07-13 15:17:39 +0700

    Added Quiz,Search,Note & Rearrange BaseBottom

--- app/src/main/java/com/example/basebottom/ui/quiz/QuizViewModel.java
+++ app/src/main/java/com/example/basebottom/ui/quiz/QuizViewModel.java
@@ -1,16 +1,16 @@
-package com.example.basebottom.ui.notifications;
+package com.example.basebottom.ui.quiz;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class QuizViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public QuizViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is quiz fragment");
     }
 
     public LiveData<String> getText() {
