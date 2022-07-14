faed9e4732a8a5308ee49eacdcae0d841758a610
0pdo1Tall <silverbullet2206@gmail.com>
2022-07-14 17:32:15 +0700

    Added Base SearchView with ListView suggestions

--- app/src/main/java/com/example/basebottom/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/basebottom/ui/search/SearchFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,9 +16,16 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.basebottom.databinding.FragmentSearchBinding;
 
+import java.util.ArrayList;
+
 public class SearchFragment extends Fragment {
 
     private FragmentSearchBinding binding;
+
+    //
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> mylist;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +35,54 @@ public class SearchFragment extends Fragment {
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSearch;
-        searchViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textSearch;
+//        searchViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        //
+        listView = binding.listView;
+        // Add items to Array List
+        mylist = new ArrayList<>();
+        mylist.add("C");
+        mylist.add("C++");
+        mylist.add("C#");
+        mylist.add("Java");
+        mylist.add("Advanced java");
+        mylist.add("Interview prep with c++");
+        mylist.add("Interview prep with java");
+        mylist.add("data structures with c");
+        mylist.add("data structures with java");
+
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, mylist);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(), (String)adapterView.getItemAtPosition(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        SearchView searchView = binding.searchView;
+        searchView.setOnQueryTextListener(
+                new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        if(mylist.contains(query)) {
+                            adapter.getFilter().filter(query);
+                        }
+                        else {
+                            Toast.makeText(getContext(), "Not found", Toast.LENGTH_SHORT).show();
+                        }
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        adapter.getFilter().filter(newText);
+                        return false;
+                    }
+                }
+        );
+
         return root;
     }
 
